919439a87c9f5a7531b9ec5b072dc30df8d84912
jsonKK <595675516@qq.com>
2019-03-28 15:39:18 +0800

    增加项目 首页静态页面搭建完成

---
+++ recycle-car/app.js
@@ -0,0 +1,4 @@
+App({
+  onShow(options){
+  }
+});
---
+++ recycle-car/pages/index/index.js
@@ -0,0 +1,11 @@
+var appUser = require('../../utils/app-user');
+Page({
+  data: {
+    titleOpacity:1
+  },
+  onLoad() {
+  },
+  onShow(){
+    
+  }
+});
