541d03a665ab65327cb23f975bc04a871a53d907
jsonKK <595675516@qq.com>
2019-03-29 18:14:15 +0800

    表单页增加

---
+++ recycle-car/pages/information/information.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    array: ['中国', '美国', '巴西', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国',
+      },
+      {
+        id: 1,
+        name: '中国',
+      },
+      {
+        id: 2,
+        name: '巴西',
+      },
+      {
+        id: 3,
+        name: '日本',
+      },
+    ],
+    arrIndex: 0,
+    index: 0
+  },
+  onLoad() {},
+});
